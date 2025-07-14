ed359a1150c66d7511d3e1faaafa0597f60f8bb4
Ubuntu <ubuntu@ip-172-31-41-221.us-east-2.compute.internal>
2025-07-14 22:36:27 +0000

    tuned hyperparams

--- src/aging_gan/train.py
+++ src/aging_gan/train.py
@@ -43,7 +43,7 @@ def parse_args() -> argparse.Namespace:
         help="Initial learning rate for discriminators.",
     )
     p.add_argument(
-        "--num_train_epochs", type=int, default=70, help="Number of training epochs."
+        "--num_train_epochs", type=int, default=80, help="Number of training epochs."
     )
     p.add_argument(
         "--train_batch_size",
