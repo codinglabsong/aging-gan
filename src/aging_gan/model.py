1eef3a2644df6d4693297a66fbaf2ea9f55f3f1e
codinglabsong <79156965+codinglabsong@users.noreply.github.com>
2025-07-19 16:31:21 -0700

    Update model.py

--- src/aging_gan/model.py
+++ src/aging_gan/model.py
@@ -29,7 +29,7 @@ class ResidualBlock(nn.Module):
 
 
 class Generator(nn.Module):
-    """U-Net style generator used for domain translation."""
+    """ResNet‑style generator used for domain translation."""
 
     def __init__(self, ngf: int, n_residual_blocks: int = 9) -> None:
         super().__init__()
